fc30421d70cd28faa1fb8b1fdaea92fcada2c2dd
Saaketh Ramoju <rsaaketh@icloud.com>
2026-06-07 17:42:23 -0400

    teeny init change

--- src/fullflow/__init__.py
+++ src/fullflow/__init__.py
@@ -5,8 +5,8 @@ FullFlow: fluid, thermal, and propulsion network simulation.
 from fullflow.System import *
 from fullflow.Solvers import *
 from fullflow.Exceptions import *
-from fullflow.Utilities import *
 
 
 def main() -> None:
-    print("Welcome to FullFlow")
+    """Run the FullFlow command-line entry point."""
+    print("FullFlow")
